ca8aee8aca78c1bbe7afe0237a1814432c8fe8fe
Andre Wolff <andre.wolff@rede.ulbra.br>
2022-10-23 11:04:29 -0300

    teste

---
+++ app/src/main/java/com/example/coletordados/Fileira03.java
@@ -0,0 +1,14 @@
+package com.example.coletordados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fileira03 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fileira03);
+    }
+}
