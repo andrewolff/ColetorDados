ddf95c0a939f680e643011a0f9bc74f2cca7dcf1
Andre Wolff <andre.wolff@rede.ulbra.br>
2022-10-23 13:15:27 -0300

    teste

--- app/src/main/java/com/example/coletordados/Lista.java
+++ app/src/main/java/com/example/coletordados/Lista.java
@@ -3,12 +3,25 @@ package com.example.coletordados;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Lista extends AppCompatActivity {
+
+    Button sair_final;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
+
+        sair_final=findViewById(R.id.sair_final);
+
+        sair_final.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
